9061b304ad6561c95ebdf4c6aef84956208d6878
Peter Cunha <petercunha8@gmail.com>
2019-09-12 16:51:33 -0700

    Update pine.py

--- pine.py
+++ pine.py
@@ -9,6 +9,6 @@ print('''
  Pine: Neural-Network Aimbot (v0.1)
 ====================================
 
-[INFO] press 'q' to quit or ctrl+C in console...''')
+[INFO] press '0' to quit or ctrl+C in console...''')
 
 pine.start(ENABLE_AIMBOT=True)
